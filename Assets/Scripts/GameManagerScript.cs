779efd6f808110e9a72e3169b15dafd56ed0b12b
Jerry Zhu <xpost2000cod@gmail.com>
2023-04-01 21:05:41 -0400

    game over menu

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -28,7 +28,33 @@ public class GameManagerScript : MonoBehaviour {
 
                 // enable the appropriate objects
                 // based off the game state.
+                print(m_state);
                 switch (m_state) {
+                    case GameState.GameWin: {
+                    } break;
+                    case GameState.GameOver: {
+                        foreach (Transform child in user_interface_container.transform) {
+                            child.gameObject.SetActive(false);
+                        }
+
+                        GameObject gameover_ui = user_interface_container.transform.Find("UIGameoverCanvas").gameObject;
+                        gameover_ui.SetActive(true);
+
+                        player.GetComponent<PlayerController>().DisableInput();
+                    } break;
+                    case GameState.Pause: {
+                    } break;
+                    case GameState.Ingame: {
+                        foreach (Transform child in user_interface_container.transform) {
+                            child.gameObject.SetActive(false);
+                        }
+
+                        GameObject gameplay_ui = user_interface_container.transform.Find("UIGameplayCanvas").gameObject;
+                        gameplay_ui.SetActive(true);
+                        player.GetComponent<PlayerController>().EnableInput();
+                    } break;
+                    case GameState.MainMenu: {
+                    } break;
                     default: {
                         print("should this be possible?");
                     } break;
@@ -38,9 +64,20 @@ public class GameManagerScript : MonoBehaviour {
     }
 
     void Start() {
-        State = GameState.Ingame;
+        print("Hi, I begin");
+        State = GameState.GameOver;
+        // State = GameState.Ingame;
 
         Application.targetFrameRate = target_framerate;
+    }
+
+    public void TryToKillGame() {
+#if UNITY_STANDALONE
+        Application.Quit();
+#endif
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 
     void Update() {}
---
+++ Assets/Scripts/UIGameOverScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGameOverScript : MonoBehaviour
+{
+    public Button restart_button;
+    public Button quit_button;
+    public Button return_to_desktop_button;
+
+    void Start() {
+        restart_button.onClick.AddListener(OnRestart);
+        quit_button.onClick.AddListener(OnQuit);
+        return_to_desktop_button.onClick.AddListener(OnReturnToDesktop);
+    }
+
+    void OnEnable() {
+        // begin my animation?
+    }
+
+    void OnRestart() {
+        print("on restart");
+    }
+
+    void OnQuit() {
+        print("on quit");
+        // should animate?
+        GameManagerScript.instance().State = GameState.MainMenu;
+    }
+
+    void OnReturnToDesktop() {
+        print("On return to desktop?");
+        GameManagerScript.instance().TryToKillGame();
+    }
+
+    void OnDisable() {} //???
+}
