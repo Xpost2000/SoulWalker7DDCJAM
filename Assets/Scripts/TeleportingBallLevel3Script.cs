9fa5c6975b4531dec5690e165e5c15d88d461409
Jerry Zhu <xpost2000cod@gmail.com>
2023-04-05 23:40:50 -0400

    Level 3 teleporters adjusted to work.

--- Assets/Scripts/TeleportingBallLevel3Script.cs
+++ Assets/Scripts/TeleportingBallLevel3Script.cs
@@ -5,37 +5,44 @@ using UnityEngine;
 public class TeleportingBallLevel3Script : MonoBehaviour
 {
     public GameObject whereto;
-    public bool do_not_teleport = false;
+
+    // hack.
+    public int do_not_teleport_for_n_frames = 0;
     // Start is called before the first frame update
     void Start() {
         
     }
 
     void OnTriggerEnter(Collider collider) {
-        if (do_not_teleport) {
+        if (do_not_teleport_for_n_frames > 0) {
+            print("I should not be teleporting!");
             return;
+        }
+
+        print(transform.parent.name);
+        print("hi, teleporting");
+
+        var teleportcomponent = whereto.transform.Find("teleballmesh").GetComponent<TeleportingBallLevel3Script>();
+        if (teleportcomponent) {
+            // prevent teleporting instantly...
+            teleportcomponent.do_not_teleport_for_n_frames = 15;
+            print("Hi, I set the other things' teleport flag to off!");
         }
 
         var gameobject = collider.gameObject;
         var controllercomponent = gameobject.GetComponent<GenericActorController>();
         if (controllercomponent) {
             controllercomponent.SetLogicalPosition(whereto.transform.position);
-        } else {
-            gameobject.transform.position = whereto.transform.position;
         }
-
-        var teleportcomponent = whereto.GetComponent<TeleportingBallLevel3Script>();
-        if (teleportcomponent) {
-            // prevent teleporting instantly...
-            teleportcomponent.do_not_teleport = true;
-        }
+        gameobject.transform.position = whereto.transform.position;
     }
     void OnTriggerExit(Collider collider) {
-        do_not_teleport = false;
+        print(transform.parent.name);
+        print("hi, being left, so untele");
     }
 
     // Update is called once per frame
     void Update() {
-        
+        do_not_teleport_for_n_frames -= 1;
     }
 }
