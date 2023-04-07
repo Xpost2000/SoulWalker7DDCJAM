181875f462093e61e62db76df1881038947164f6
Jerry Zhu <xpost2000cod@gmail.com>
2023-04-07 14:34:28 -0400

    ExpositionTutorialPromptOpenerScript.cs created
    This is for the tutorial message triggers, just in case things go wrong!

---
+++ Assets/Scripts/ExpositionTutorialPromptOpenerScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ExpositionTutorialPromptOpenerScript : MonoBehaviour {
+    public string text;
+    public float readspeed = 0.05f;
+    public float delaytime = 1.0f;
+
+    void Start() {}
+    void Update() {}
+
+    void OnTriggerEnter(Collider collider) {
+        var collider_gameobject = collider.gameObject;
+
+        if (collider_gameobject.tag == "Player") {
+            GameManagerScript.instance().OpenExpositionTutorialPanel(
+                text,
+                readspeed,
+                delaytime
+            );
+
+            // destroy self, no longer needed!
+            // I hope.
+            Destroy(gameObject);
+        }
+    }
+}
